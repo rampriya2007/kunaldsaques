66b2d2ff2136c3ded7ec1f2206a6dc9bd0bae289
rampriya2007 <ramuankgm2105@email,com>
2026-03-17 07:09:24 +0530

    added conditional_loop

---
+++ src/conditional_loops.java
@@ -0,0 +1,223 @@
+public class conditional_loops
+{
+    //Area Of Circle Java Program
+    void one(){
+        int r=3;
+        System.out.println(3.14*Math.pow(r,2));
+    }
+    //Area Of Triangle
+    void two(){
+        int h=4,b=6;
+        System.out.println((1/2)*h*b);
+    }
+    //Area Of Rectangle Program
+    void three(){
+ int l=5,w=8;
+        System.out.println(l*w);
+    }
+    //Area Of Isosceles Triangle
+    void four(){
+        int a=3,b=2;
+        System.out.println((2/4)*(4*(Math.pow(a,2))-Math.pow(b,2)));
+    }
+    //Area Of Parallelogram
+    void five(){
+int b=3,h=5;
+        System.out.println(b*h);
+    }
+    //Area Of Rhombus
+    void six(){
+
+    }
+    //Area Of Equilateral Triangle
+    void sven(){
+
+    }
+    //Perimeter Of Circle
+    void eight(){
+
+    }
+    //Perimeter Of Equilateral Triangle
+    void nine(){
+
+    }
+    //Perimeter Of Parallelogram
+    void zero(){
+
+    }
+    //Perimeter Of Rectangle
+    void eleven(){
+
+    }
+    //Perimeter Of Square
+    void twelve(){
+
+    }
+    //Perimeter Of Rhombus
+    void thirteen(){
+
+    }
+    //Volume Of Cone Java Program
+    void fourteen(){
+
+    }
+    //Volume Of Prism
+    void fifteen(){
+
+    }
+    //Volume Of Cylinder
+    void sixteen(){
+
+    }
+    //Volume Of Sphere
+    void seventeen(){
+
+    }
+    //Volume Of Pyramid
+    void eighteen(){
+
+    }
+    //Curved Surface Area Of Cylinder
+    void ninteen(){
+
+    }
+    //Total Surface Area Of Cube
+    void twnty(){
+
+    }
+    //Fibonacci Series In Java Programs
+    void twenty1(){
+
+    }
+    //Subtract the Product and Sum of Digits of an Integer
+    void twenty2(){
+
+    }
+    //Input a number and print all the factors of that number (use loops).
+    void twenty3(){
+
+    }
+    //Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+    void twenty4(){
+
+    }
+    //Take integer inputs till the user enters 0 and print the largest number from all.
+    void twenty5(){
+
+    }
+    //Addition Of Two Numbers
+    void twenty6(){
+
+    }
+    //Intermediate Java Programs
+    void twenty7(){
+
+    }
+    //Factorial Program In Java
+    void twenty8(){
+
+    }
+    //Calculate Electricity Bill
+    void twenty9(){
+
+    }
+    //Calculate Average Of N Numbers
+    void twenty19(){
+
+    }
+    //Calculate Discount Of Product
+    void thirty(){
+
+    }
+    //Calculate Distance Between Two Points
+    void thirty1(){
+
+    }
+    //Calculate Commission Percentage
+    void thirty2(){
+
+    }
+    //Power In Java
+    void thirty3(){
+
+    }
+    //Calculate Depreciation of Value
+    void thirty4(){
+
+    }
+    //Calculate Batting Average
+    void thirty5(){
+
+    }
+
+    //Calculate CGPA Java Program
+    void thirty6(){
+
+    }
+    //Compound Interest Java Program
+    void thirty7(){
+
+    }
+    //Calculate Average Marks
+    void thirty8(){
+
+    }
+    //Sum Of N Numbers
+    void thirty9(){
+
+    }
+    //Armstrong Number In Java
+    void forty(){
+
+    }
+    //Find Ncr & Npr
+    void forty1(){
+
+    }
+    //Reverse A String In Java
+    void forty2(){
+
+    }
+    //Find if a number is palindrome or not
+    void forty3(){
+
+    }
+    //Future Investment Value
+    void forty4(){
+
+    }
+    //HCF Of Two Numbers Program
+    void forty5(){
+
+    }
+    //LCM Of Two Numbers
+    void forty6(){
+
+    }
+    //Java Program Vowel Or Consonant
+    void forty7(){
+
+    }
+    //Perfect Number In Java
+    void forty8(){
+
+    }
+    //Check Leap Year Or Not
+    void forty9(){
+
+    }
+
+    //Sum Of A Digits Of Number
+    void fivty(){
+
+    }
+    //Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+    void fivty1(){
+
+    }
+    //Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.
+    void fivty2(){
+
+    }
+}
+
